45e7ba321bef2da9940e361ec7493c674cc8505f
Benjamin Diolez <bdiolez@gmail.com>
2015-12-08 16:05:48 +0100

    Add localStorage support

--- visitCounter.js
+++ visitCounter.js
@@ -1,15 +1,17 @@
-var _cookieName = "siteVisits";
+var _storageName = "siteVisits";
 var _cookieDomain = ".domain.com";
 var _cookieDuration = 365; // days
 var _visitLifetime = 30; // minutes
+var _allowLocalStorage = true;
 
+var _storageEnabled = (_allowLocalStorage && typeof Storage !== void(0)) ? true : false;
 function getWeekNumber(e){e=new Date(+e),e.setHours(0,0,0),e.setDate(e.getDate()+4-(e.getDay()||7));var t=new Date(e.getFullYear(),0,1),a=Math.ceil(((e-t)/864e5+1)/7);return a}
 function setCookie(e,t,i,o){var n=new Date;n.setTime(n.getTime()+24*i*60*60*1e3);var a="expires="+n.toUTCString();document.cookie=e+"="+t+"; "+a+";domain="+o+";path=/"}
 function getCookie(t){for(var n=t+"=",r=document.cookie.split(";"),e=0;e<r.length;e++){for(var i=r[e];" "==i.charAt(0);)i=i.substring(1);if(0==i.indexOf(n))return i.substring(n.length,i.length)}return""}
 
 var storedVisits = (function(){
   // get current cookie value
-  var initCookie = getCookie(_cookieName);
+  var initContent = (_storageEnabled) ? localStorage.getItem(_storageName) : getCookie(_storageName);
   var d = new Date();
   var currentYear = d.getFullYear();
   var currentMonth = d.getMonth();
@@ -27,8 +29,8 @@ var storedVisits = (function(){
   };
   
   // cookie exists
-  if(initCookie != "") {
-    oldVisits = JSON.parse(initCookie);
+  if(initContent != "" && initContent != null) {
+    oldVisits = JSON.parse(initContent);
     oldLastPage = new Date(oldVisits.lastpage);
     oldLastVisit = new Date(oldVisits.lastvisitstart);
   
@@ -54,7 +56,7 @@ var storedVisits = (function(){
   }
   
   // set cookie with new values
-  setCookie(_cookieName, JSON.stringify(siteVisits), _cookieDuration, _cookieDomain);
+  (_storageEnabled) ? localStorage.setItem(_storageName, JSON.stringify(siteVisits)) : setCookie(_storageName, JSON.stringify(siteVisits), _cookieDuration, _cookieDomain);
   
   return siteVisits.visits;
 })();
